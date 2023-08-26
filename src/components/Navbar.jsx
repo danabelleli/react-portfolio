9db5971c18fac318186ac47dad889d94a00bef97
dannabelleli <belleli.danna@gmail.com>
2023-08-26 09:00:27 +0300

    added starter files. also added code to the main, app and navbar files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link, useLocation } from 'react-router-dom';
+
+function Navbar() {
+    const currentPage = useLocation().pathname;
+
+    return (
+
+        <ul className='nav'>
+            <li className='nav-item'>
+                <Link to="/" className={currentPage === '/' ? 'nav-link active' : 'nav-link'}>About</Link>
+            </li>
+            <li className='nav-item'>
+                <Link to="/Contact" className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}>Contact</Link>
+            </li>
+            <li className='nav-item'>
+                <Link to="/Portfolio" className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</Link>
+            </li>
+            <li className='nav-item'>
+                <Link to="/Resume" className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}>Resume</Link>
+            </li>
+        </ul>
+    )
+}
+
+export default Navbar;
