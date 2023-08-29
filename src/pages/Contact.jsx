bb10727677914f7f251df36a09a0b760c20ea5e6
dannabelleli <belleli.danna@gmail.com>
2023-08-30 02:58:00 +0300

    modified code in the contact page. changed last name input to an email input;2A

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,9 +16,9 @@ export default function Contact() {
                             </div>
                         </div>
                         <div>
-                            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
+                            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">Email</label>
                             <div className="mt-2.5">
-                                <input type="text" name="last-name" id="last-name" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+                                <input type="email" name="email" id="email" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
                             </div>
                         </div>
 
